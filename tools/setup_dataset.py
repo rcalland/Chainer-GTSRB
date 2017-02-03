0d922fb3d48880b0aa4829743419bdaea630e6cf
rcalland <richard.calland@ipmu.jp>
2017-02-03 18:30:04 +0900

    script to download, unzip and register data path in the config file

---
+++ tools/setup_dataset.py
@@ -0,0 +1,46 @@
+import os
+import argparse
+from tqdm import tqdm
+import requests
+import zipfile
+from utils import load_config, save_config
+
+gtsrb_url = "http://benchmark.ini.rub.de/Dataset/GTSRB_Final_Training_Images.zip"
+gtsrb_test_url = "http://benchmark.ini.rub.de/Dataset/GTSRB_Final_Test_Images.zip"
+gtsrb_test_annotations_url = "http://benchmark.ini.rub.de/Dataset/GTSRB_Final_Test_GT.zip"
+
+def download(url, local_path):
+    response = requests.get(url, stream=True)
+    local_path = os.path.join(local_path, os.path.basename(url))
+    with open(local_path, "wb") as handle:
+        for data in tqdm(response.iter_content(chunk_size=1024)):
+            if data:
+                handle.write(data)
+    return local_path
+
+def unzip(local_file):
+    data_path = os.path.dirname(os.path.abspath(local_file))
+    zip_ref = zipfile.ZipFile(local_file, 'r')
+    zip_ref.extractall(data_path)
+    zip_ref.close()
+    return data_path
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_dir", "-o", type=str, required=True)
+    parser.add_argument("--data_url", "-i", type=str, default=gtsrb_url)
+    return parser.parse_args()
+
+def set_config(config_file, base_dir):
+    cfg = load_config(config_file)
+    cfg["data_root_path"] = base_dir
+    save_config(config_file, cfg)
+
+def main():
+    args = parse_arguments()
+    local_file = download(args.data_url, args.data_dir)
+    base_dir = unzip(local_file)
+    set_config("../config/gtsrb.json", base_dir)
+    
+if __name__=="__main__":
+    main()
