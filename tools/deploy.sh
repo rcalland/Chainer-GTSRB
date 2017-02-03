e6e76b62e49a75ab7ddcbfd0ed142d55449da623
rcalland <richard.calland@ipmu.jp>
2017-02-03 19:31:21 +0900

    uncomment the download script!

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -2,7 +2,7 @@
 
 if [ -z ${1+x} ]; then echo "You need to specify the data path, e.g. source deploy.sh /home/user/data"; return; else echo "Setting up data in ${1}"; fi
 
-#python setup_dataset.py -o $1
+python setup_dataset.py -o $1
 python oversampler.py
 python make_training_annotations.py
 python generate_means.py
