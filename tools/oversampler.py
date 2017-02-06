e6779175e5065b0f914c9fdab860ed2556087351
rcalland <richard.calland@ipmu.jp>
2017-02-06 17:31:15 +0900

    changed sampling scheme, so now a set number of augmentations is determined per class, then random images are selected from the class for each augmentation iteration. Results in a more unbiased distribution of samples

--- tools/oversampler.py
+++ tools/oversampler.py
@@ -11,13 +11,13 @@ import operator
 import math
 from utils import load_config
 
-def make_augmentations(img_path, num_copies):
+def make_augmentations(img_path, _id = "", num_copies=1):
     # taken from http://docs.opencv.org/trunk/da/d6e/tutorial_py_geometric_transformations.html
    
-    pixel_jitter = 4
-    rotate_degrees = 25
-    scale_min = 0.8
-    scale_max = 1.2
+    pixel_jitter = 2
+    rotate_degrees = 15
+    scale_min = 0.9
+    scale_max = 1.1
 
     img = cv2.imread(img_path)
     for i in range(num_copies):
@@ -32,7 +32,7 @@ def make_augmentations(img_path, num_copies):
         dst = cv2.warpAffine(img,M,(cols,rows))
         
         # save this image
-        newname = "{}_oversampled_{}.ppm".format(img_path, i)
+        newname = "{}_oversampled_{}_{}.ppm".format(img_path, _id, i)
         cv2.imwrite(newname, dst)
 
 def get_class(path):
@@ -50,6 +50,7 @@ def fetch_list(base_dir):
 
     sample_list = []
     label_list = []
+    pair = []
     print(base_dir)
 
     #for root, dirs, files in os.walk(base_dir, topdown=False):
@@ -68,16 +69,17 @@ def fetch_list(base_dir):
 
             elif f.endswith(".ppm"):
                 lbl = get_class(class_path)
-                sample_list.append(full_path)
-                label_list.append(lbl)
+                pair.append([full_path, lbl])
+                #sample_list.append(full_path)
+                #label_list.append(lbl)
 
     print("done.")
-    return sample_list, label_list
+    return pair #sample_list, label_list
 
 def class_frequency(labels):
-    return collections.OrderedDict(collections.Counter(labels))
+    return collections.OrderedDict(collections.Counter(x[1] for x in labels))#labels))
 
-def upsample(freq, sample_list, label_list, global_scale=0):
+def upsample(freq, sample_list, global_scale=0):
     print("beginning upsampling...")
     # get the class with the most samples
     maxkey = max(freq.iteritems(), key=operator.itemgetter(1))[0]
@@ -86,13 +88,25 @@ def upsample(freq, sample_list, label_list, global_scale=0):
 
     total_files = len(sample_list)
 
+    print(freq)
+
     # perform sampling
-    for i, (spl, lbl) in enumerate(zip(sample_list, label_list)):
-        if i % 1000 is 0:
-            print("{} / {}".format(i, total_files))
-        num_augmentations = maxsamples - freq[lbl]
-        num_augmentations = int(math.ceil(num_augmentations / float(freq[lbl]))) + int(maxsamples * global_scale)
-        make_augmentations(spl, num_augmentations)
+    for key, val in freq.iteritems():
+        #print(key, val)
+    #for i, (spl, lbl) in enumerate(zip(sample_list, label_list)):
+    #    if i % 1000 is 0:
+        #     print("{} / {}".format(i, total_files))
+    
+        num_augmentations = maxsamples - val
+        num_augmentations = num_augmentations + int(maxsamples * global_scale)
+        print("{} upsampling to {} for class {}".format(val, val + num_augmentations, key))
+
+        # select only same-class imaes
+        rndm = [x[0] for x in sample_list if x[1] == key]
+        
+        for aug in range(num_augmentations):
+            make_augmentations(random.choice(rndm), _id=str(aug))
+    
     print("done.")
 
 def main():
@@ -105,11 +119,11 @@ def main():
         cfg = load_config("../config/gtsrb.json")
         args.base_dir = cfg["data_root_path"]
 
-    sample_list, label_list = fetch_list(args.base_dir)
-    freq = class_frequency(label_list)
+    pair = fetch_list(args.base_dir)
+    freq = class_frequency(pair)
     
     # global_scale increases all class samples by %
-    upsample(freq, sample_list, label_list, 0.0)
+    upsample(freq, pair, 0.0)
 
 if __name__=="__main__":
     main()
